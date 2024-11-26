780b05dc46f83fab48bc36ab083b39d0d85c2e87
root <hs.park@strato.co.kr>
2024-11-26 15:16:11 +0900

    결과 공통 처리

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.MemberDto;
-import com.example.demo.entity.MemberEntity;
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.enumeration.ResultCodeEnum;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,15 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-
     @Autowired
     private MemberService memberService;
 
     @PostMapping("/member/info")
-    public MemberDto getMemberInfo(@RequestBody MemberDto.MemberRequestDto req) {
-        int memberId = req.getId();
-        MemberEntity returnMember = memberService.getMemberById(memberId);
-        MemberDto toDto = MemberDto.of(returnMember);
-        return toDto;
+    public ResponseDto<MemberDto> getMemberInfo(@RequestBody MemberDto.MemberRequestDto req) {
+        MemberDto toDto = MemberDto.of(memberService.getMemberById(req.getId()));
+        return ResponseDto.of(String.valueOf(ResultCodeEnum.getEnum(200).getCode())
+                ,ResultCodeEnum.getEnum(200).getMessage()
+                ,toDto);
     }
 }

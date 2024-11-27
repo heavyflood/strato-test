b1454b028b0a4d951319097070a6bf1e9bd38d94
root <hs.park@strato.co.kr>
2024-11-27 13:32:44 +0900

    결과 공통 처리

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.GeneralException;
 import com.example.demo.dto.MemberDto;
 import com.example.demo.dto.ResponseDto;
+import com.example.demo.entity.MemberEntity;
 import com.example.demo.enumeration.ResultCodeEnum;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class MemberController {
@@ -16,9 +20,12 @@ public class MemberController {
 
     @PostMapping("/member/info")
     public ResponseDto<MemberDto> getMemberInfo(@RequestBody MemberDto.MemberRequestDto req) {
-        MemberDto toDto = MemberDto.of(memberService.getMemberById(req.getId()));
+        MemberEntity entity =
+                Optional.ofNullable(memberService.getMemberById(req.getId()))
+                    .orElseThrow(() -> new GeneralException(ResultCodeEnum.getEnum(40001)));
+
         return ResponseDto.of(String.valueOf(ResultCodeEnum.getEnum(200).getCode())
                 ,ResultCodeEnum.getEnum(200).getMessage()
-                ,toDto);
+                ,MemberDto.of(entity));
     }
 }
